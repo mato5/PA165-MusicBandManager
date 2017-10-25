66c9e034d77b6d232df3179ae297e59e9a4aa8cd
xkuznetc <466672@mail.muni.cz>
2017-10-26 00:22:40 +0200

    Band entity: instanceof replaces getClass by an objects comprasion.

--- bandmanager-persistance/src/main/java/cz/muni/fi/pa165/entity/Band.java
+++ bandmanager-persistance/src/main/java/cz/muni/fi/pa165/entity/Band.java
@@ -101,7 +101,7 @@ public class Band {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || !(o instanceof Band)) return false;
 
         Band band = (Band) o;
 
