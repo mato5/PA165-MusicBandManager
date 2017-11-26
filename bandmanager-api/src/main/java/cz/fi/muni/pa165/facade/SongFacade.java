061fdf186c4d9ebb7e67b64879617da8f911276a
xkuznetc <466672@mail.muni.cz>
2017-11-26 13:39:32 +0100

    SongFacade and dto added.

---
+++ bandmanager-api/src/main/java/cz/fi/muni/pa165/facade/SongFacade.java
@@ -0,0 +1,29 @@
+package cz.fi.muni.pa165.facade;
+
+import cz.fi.muni.pa165.dto.BandDTO;
+import cz.fi.muni.pa165.dto.SongCreateDTO;
+import cz.fi.muni.pa165.dto.SongDTO;
+
+import java.util.Collection;
+
+/**
+ * @author Iurii xkuznetc Kuznetcov
+ */
+
+public interface SongFacade {
+
+    public SongDTO findSongById(Long id);
+
+    public Collection<SongDTO> findAll();
+
+    public Collection<SongDTO> findSongsByName(String name);
+
+    public Collection<SongDTO> findSongsByBand(BandDTO band);
+
+    public Long createSong(SongCreateDTO songCreateDTO);
+
+    public void changeDuration(SongDTO songDTO, Long newsongDuration);
+
+    public void changeBand(SongDTO songDTO, BandDTO newBandDTO);
+
+}
