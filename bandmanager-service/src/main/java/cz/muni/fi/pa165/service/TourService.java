ecdc5370fbaa8a8c12607cd1a6ff30d1143a02af
Alexander Kromka <n3v3rd1e6@gmail.com>
2017-11-27 21:22:47 +0100

    Vytvoreny TourService

---
+++ bandmanager-service/src/main/java/cz/muni/fi/pa165/service/TourService.java
@@ -0,0 +1,29 @@
+package cz.muni.fi.pa165.service;
+
+import cz.muni.fi.pa165.entity.Band;
+import cz.muni.fi.pa165.entity.Manager;
+import cz.muni.fi.pa165.entity.Tour;
+
+import java.util.List;
+
+public interface TourService {
+    public Tour create(Tour tour);
+
+    public List<Tour> findAll();
+
+    public Tour findById(Long id);
+
+    public List<Tour> findByManager(Manager manager);
+
+    public List<Tour> findByBand(Band band);
+
+    public Tour setName(Tour tour, String name);
+
+    public Tour setCity(Tour tour, String name);
+
+    public Tour setManager(Tour tour, Manager manager);
+
+    public Tour setBand(Tour tour, Band band);
+
+    public void delete(Tour tour);
+}
