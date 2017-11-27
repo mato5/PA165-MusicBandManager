7b12d2b470b237c83b4c11b1aff0aebeed02677b
Alexander Kromka <n3v3rd1e6@gmail.com>
2017-11-27 21:19:38 +0100

    Spraveny TourDTO

--- bandmanager-api/src/main/java/cz/fi/muni/pa165/dto/TourDTO.java
+++ bandmanager-api/src/main/java/cz/fi/muni/pa165/dto/TourDTO.java
@@ -1,7 +1,63 @@
 package cz.fi.muni.pa165.dto;
 
 /**
- * TODO!
+ * @author Alexander Kromka
  */
 public class TourDTO {
+    private Long id;
+    private BandDTO band;
+    private ManagerDTO manager;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BandDTO getBand() {
+        return band;
+    }
+
+    public void setBand(BandDTO band) {
+        this.band = band;
+    }
+
+    public ManagerDTO getManager() {
+        return manager;
+    }
+
+    public void setManager(ManagerDTO manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof TourDTO)) return false;
+
+        TourDTO tourDTO = (TourDTO) o;
+
+        if (id != null ? !id.equals(tourDTO.getId()) : tourDTO.getId() != null) return false;
+        if (manager != null ? !manager.equals(tourDTO.getManager()) : tourDTO.getManager() != null) return false;
+        return band != null ? band.equals(tourDTO.getBand()) : tourDTO.getBand() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (band != null ? band.hashCode() : 0);
+        result = 31 * result + (manager != null ? manager.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TourDTO{" +
+                "id=" + id +
+                ", band=" + band +
+                ", manager=" + manager +
+                '}';
+    }
 }
