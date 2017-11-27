20831f4bb1b86af5f253f824c93333fef5ed5ffb
kami <kami@mycroftmind.com>
2017-11-27 01:50:02 +0100

    + facade layer for Band implemented

---
+++ bandmanager-api/src/main/java/cz/fi/muni/pa165/facade/BandFacade.java
@@ -0,0 +1,35 @@
+package cz.fi.muni.pa165.facade;
+
+import cz.fi.muni.pa165.dto.BandCreateDTO;
+import cz.fi.muni.pa165.dto.BandDTO;
+import cz.fi.muni.pa165.dto.ManagerDTO;
+import cz.fi.muni.pa165.dto.MemberDTO;
+import cz.muni.fi.pa165.enums.Genre;
+import java.util.List;
+
+/**
+ *
+ * @author kami
+ */
+public interface BandFacade {
+
+    public BandDTO findById(Long idDTO);
+
+    public Long create(BandCreateDTO bandDTO);
+
+    public void delete(BandDTO bandDTO);
+
+    public List<BandDTO> findAll();
+
+    public List<BandDTO> findByManager(ManagerDTO managerDTO);
+ 
+    public List<BandDTO> findByGenre(Genre genreDTO);
+
+    /*public void addMember(BandDTO band, MemberDTO memberDTO);
+
+    public void removeMember(BandDTO band, MemberDTO memberDTO);
+    */
+    public void changeManager(BandDTO band, ManagerDTO managerDTO);
+
+    public void changeGenre(BandDTO band, Genre genre);
+}
