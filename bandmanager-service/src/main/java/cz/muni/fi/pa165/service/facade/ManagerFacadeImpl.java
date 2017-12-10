930a68393e7e6fc32ac1995f108d7644ed2ed641
mato5 <matej.sojak@gmail.com>
2017-12-10 23:31:45 +0100

    Member Facade overhaul

--- bandmanager-service/src/main/java/cz/muni/fi/pa165/service/facade/ManagerFacadeImpl.java
+++ bandmanager-service/src/main/java/cz/muni/fi/pa165/service/facade/ManagerFacadeImpl.java
@@ -46,93 +46,92 @@ public class ManagerFacadeImpl implements ManagerFacade {
     private TourService tourService;
 
 
-
     @Override
     public void registerManager(ManagerDTO m, String unencryptedPassword) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        managerService.registerManager(manager,unencryptedPassword);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        managerService.registerManager(manager, unencryptedPassword);
         m.setId(manager.getId());
     }
 
     @Override
     public Collection<ManagerDTO> getAllManagers() {
-        return beanMappingService.mapTo(managerService.getAllManagers(),ManagerDTO.class);
+        return beanMappingService.mapTo(managerService.getAllManagers(), ManagerDTO.class);
     }
 
     @Override
     public ManagerDTO findManagerById(Long id) {
         Manager manager = managerService.findManagerById(id);
-        return (manager == null) ? null : beanMappingService.mapTo(manager,ManagerDTO.class);
+        return (manager == null) ? null : beanMappingService.mapTo(manager, ManagerDTO.class);
     }
 
     @Override
     public ManagerDTO findManagerByName(String name) {
         Manager manager = managerService.findManagerByName(name);
-        return (manager == null) ? null : beanMappingService.mapTo(manager,ManagerDTO.class);
+        return (manager == null) ? null : beanMappingService.mapTo(manager, ManagerDTO.class);
     }
 
     @Override
     public boolean authenticate(UserAuthDTO u) {
-        return managerService.authenticate(managerService.findManagerById(u.getId()),u.getPassword());
+        return managerService.authenticate(managerService.findManagerById(u.getId()), u.getPassword());
     }
 
     @Override
     public void changeEmail(UserChangeEmailDTO u) {
         Manager manager = managerService.findManagerById(u.getId());
-        managerService.changeEmail(manager,u.getEmail());
+        managerService.changeEmail(manager, u.getEmail());
     }
 
     @Override
     public void changePassword(UserChangePasswordDTO u) {
         Manager manager = managerService.findManagerById(u.getId());
-        managerService.changePassword(manager,u.getPassword());
+        managerService.changePassword(manager, u.getPassword());
     }
 
     @Override
     public void addManagedBand(ManagerDTO m, BandDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        Band band = beanMappingService.mapTo(b,Band.class);
-        managerService.addManagedBand(manager,band);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        Band band = beanMappingService.mapTo(b, Band.class);
+        managerService.addManagedBand(manager, band);
     }
 
     @Override
     public void cancelManagedBand(ManagerDTO m, BandDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        Band band = beanMappingService.mapTo(b,Band.class);
-        managerService.cancelManagedBand(manager,band);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        Band band = beanMappingService.mapTo(b, Band.class);
+        managerService.cancelManagedBand(manager, band);
     }
 
     @Override
     public void addTour(ManagerDTO m, TourDTO t) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        Tour tour = beanMappingService.mapTo(t,Tour.class);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        Tour tour = beanMappingService.mapTo(t, Tour.class);
         Band band = tour.getBand();
         tourService.create(tour);
-        managerService.addTour(manager,tour);
-        bandService.addTour(band,tour);
+        managerService.addTour(manager, tour);
+        bandService.addTour(band, tour);
     }
 
     @Override
     public void cancelTour(ManagerDTO m, TourDTO t) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        Tour tour = beanMappingService.mapTo(t,Tour.class);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        Tour tour = beanMappingService.mapTo(t, Tour.class);
         Band band = tour.getBand();
-        managerService.cancelTour(manager,tour);
-        bandService.cancelTour(band,tour);
+        managerService.cancelTour(manager, tour);
+        bandService.cancelTour(band, tour);
     }
 
     @Override
     public void addBandInvite(ManagerDTO m, BandInviteDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        BandInvite invite = beanMappingService.mapTo(b,BandInvite.class);
-        managerService.addBandInvite(manager,invite);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        BandInvite invite = beanMappingService.mapTo(b, BandInvite.class);
+        managerService.addBandInvite(manager, invite);
     }
 
     @Override
     public void cancelBandInvite(ManagerDTO m, BandInviteDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        BandInvite invite = beanMappingService.mapTo(b,BandInvite.class);
-        managerService.cancelBandInvite(manager,invite);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        BandInvite invite = beanMappingService.mapTo(b, BandInvite.class);
+        managerService.cancelBandInvite(manager, invite);
     }
 
 
@@ -140,51 +139,51 @@ public class ManagerFacadeImpl implements ManagerFacade {
 
     @Override
     public void createBand(ManagerDTO m, BandCreateDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        Band newBand = beanMappingService.mapTo(b,Band.class);
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        Band newBand = beanMappingService.mapTo(b, Band.class);
         bandService.create(newBand);
-        managerService.addManagedBand(manager,newBand);
+        managerService.addManagedBand(manager, newBand);
     }
 
     @Override
     public void sendBandInvite(ManagerDTO m, BandInviteDTO b) {
-        Manager manager = beanMappingService.mapTo(m,Manager.class);
-        BandInvite invite = beanMappingService.mapTo(b,BandInvite.class);
-        Member member =  invite.getInvitedMember();
+        Manager manager = beanMappingService.mapTo(m, Manager.class);
+        BandInvite invite = beanMappingService.mapTo(b, BandInvite.class);
+        Member member = invite.getInvitedMember();
         bandInviteService.create(invite);
-        managerService.addBandInvite(manager,invite);
-        memberService.sendBandInvite(member,invite);
+        managerService.addBandInvite(manager, invite);
+        memberService.sendBandInvite(member, invite);
     }
 
     @Override
     public void changeBandGenre(ManagerDTO m, BandGengreDTO b) {
         Band band = bandService.findById(b.getId());
-        bandService.changeGenre(band,b.getGenre());
+        bandService.changeGenre(band, b.getGenre());
     }
 
     @Override
     public void changeBandName(ManagerDTO m, BandNameDTO b) {
         Band band = bandService.findById(b.getId());
-        bandService.changeName(band,b.getName());
+        bandService.changeName(band, b.getName());
     }
 
     @Override
     public void changeBandLogo(ManagerDTO m, BandLogoDTO b) {
         Band band = bandService.findById(b.getId());
-        bandService.changeLogoUri(band,b.getLogoURI());
+        bandService.changeLogoUri(band, b.getLogoURI());
     }
 
     //NOT SURE ABOUT THIS
     @Override
     public void addNewSong(ManagerDTO m, SongCreateDTO s) {
-        Song newSong = beanMappingService.mapTo(s,Song.class);
+        Song newSong = beanMappingService.mapTo(s, Song.class);
         songService.create(newSong);
     }
 
     //NOT SURE ABOUT THIS
     @Override
     public void addNewAlbum(ManagerDTO m, AlbumCreateDTO a) {
-        Album newAlbum = beanMappingService.mapTo(a,Album.class);
+        Album newAlbum = beanMappingService.mapTo(a, Album.class);
         albumService.create(newAlbum);
     }
 }
--- bandmanager-service/src/main/java/cz/muni/fi/pa165/service/facade/MemberFacadeImpl.java
+++ bandmanager-service/src/main/java/cz/muni/fi/pa165/service/facade/MemberFacadeImpl.java
@@ -1,14 +1,11 @@
 package cz.muni.fi.pa165.service.facade;
 
-import cz.fi.muni.pa165.dto.MemberDTO;
-import cz.fi.muni.pa165.dto.UserAuthDTO;
-import cz.fi.muni.pa165.dto.UserChangeEmailDTO;
-import cz.fi.muni.pa165.dto.UserChangePasswordDTO;
+import cz.fi.muni.pa165.dto.*;
 import cz.fi.muni.pa165.facade.MemberFacade;
+import cz.muni.fi.pa165.entity.Band;
 import cz.muni.fi.pa165.entity.BandInvite;
 import cz.muni.fi.pa165.entity.Member;
-import cz.muni.fi.pa165.service.BeanMappingService;
-import cz.muni.fi.pa165.service.MemberService;
+import cz.muni.fi.pa165.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -29,73 +26,113 @@ public class MemberFacadeImpl implements MemberFacade {
     @Inject
     private BeanMappingService beanMappingService;
 
+    @Inject
+    private BandService bandService;
+
+    @Inject
+    private BandInviteService bandInviteService;
+
+    @Inject
+    private TourService tourService;
+
     @Override
     public MemberDTO findMemberById(Long id) {
         Member member = memberService.findMemberById(id);
-        return (member == null) ? null : beanMappingService.mapTo(member,MemberDTO.class);
+        return (member == null) ? null : beanMappingService.mapTo(member, MemberDTO.class);
     }
 
     @Override
     public MemberDTO findMemberByEmail(String email) {
         Member member = memberService.findMemberByEmail(email);
-        return (member == null) ? null : beanMappingService.mapTo(member,MemberDTO.class);
+        return (member == null) ? null : beanMappingService.mapTo(member, MemberDTO.class);
     }
 
     @Override
     public void registerMember(MemberDTO m, String unencryptedPassword) {
-        Member member = beanMappingService.mapTo(m,Member.class);
-        memberService.registerMember(member,unencryptedPassword);
+        Member member = beanMappingService.mapTo(m, Member.class);
+        memberService.registerMember(member, unencryptedPassword);
         m.setId(member.getId());
     }
 
     @Override
     public Collection<MemberDTO> getAllMembers() {
-        return beanMappingService.mapTo(memberService.getAllMembers(),MemberDTO.class);
+        return beanMappingService.mapTo(memberService.getAllMembers(), MemberDTO.class);
     }
 
     @Override
     public boolean authenticate(UserAuthDTO u) {
-        return memberService.authenticate(memberService.findMemberById(u.getId()),u.getPassword());
+        return memberService.authenticate(memberService.findMemberById(u.getId()), u.getPassword());
     }
 
     @Override
     public void changeEmail(UserChangeEmailDTO u) {
         Member member = memberService.findMemberByEmail(u.getEmail());
-        memberService.changeEmail(member,u.getEmail());
+        memberService.changeEmail(member, u.getEmail());
     }
 
     @Override
     public void changePassword(UserChangePasswordDTO u) {
         Member member = memberService.findMemberById(u.getId());
-        memberService.changePassword(member,u.getPassword());
+        memberService.changePassword(member, u.getPassword());
     }
 
     @Override
     public Collection<MemberDTO> findMemberByName(String name) {
-        return beanMappingService.mapTo(memberService.findMemberByName(name),MemberDTO.class);
+        return beanMappingService.mapTo(memberService.findMemberByName(name), MemberDTO.class);
     }
 
     @Override
     public void acceptBandInvite(Long userId, Long bandInvId) {
-        Member m =  memberService.findMemberById(userId);
+        Member m = memberService.findMemberById(userId);
         BandInvite b = null;
-        for(BandInvite inv : m.getBandInvites()){
-            if(inv.getId() == bandInvId){
+        Band band = null;
+        for (BandInvite inv : m.getBandInvites()) {
+            if (inv.getId() == bandInvId) {
                 b = inv;
+                band = inv.getBand();
             }
         }
-        memberService.acceptBandInvite(m,b);
+        memberService.acceptBandInvite(m, b);
+        bandService.addMember(band, m);
+        bandInviteService.delete(b);
     }
 
     @Override
     public void declineBandInvite(Long userId, Long bandInvId) {
         Member m = memberService.findMemberById(userId);
         BandInvite b = null;
-        for(BandInvite inv : m.getBandInvites()){
-            if(inv.getId() == bandInvId){
+        for (BandInvite inv : m.getBandInvites()) {
+            if (inv.getId() == bandInvId) {
                 b = inv;
             }
         }
-        memberService.declineBandInvite(m,b);
+        memberService.declineBandInvite(m, b);
+        bandInviteService.delete(b);
+    }
+
+    @Override
+    public Collection<BandInviteDTO> listAllMemberInvites(Long memberId) {
+        Member m = memberService.findMemberById(memberId);
+        return beanMappingService.mapTo(bandInviteService.findByMember(m), BandInviteDTO.class);
+    }
+
+    @Override
+    public Collection<MemberDTO> listBandmates(Long memberId) {
+        Member m = memberService.findMemberById(memberId);
+        if (m.getBand() == null) {
+            throw new IllegalArgumentException("This user is not a member of any band!");
+        }
+        Band b = bandService.findById(m.getBand().getId());
+        return beanMappingService.mapTo(b.getMembers(), MemberDTO.class);
+    }
+
+    @Override
+    public Collection<TourDTO> listAllActivities(Long memberId) {
+        Member m = memberService.findMemberById(memberId);
+        if (m.getBand() == null) {
+            throw new IllegalArgumentException("This user is not a member of any band!");
+        }
+        Band b = bandService.findById(m.getBand().getId());
+        return beanMappingService.mapTo(tourService.findByBand(b), TourDTO.class);
     }
 }
