1d883cc8b571ceb74adf6d5c46753be2b1f409c3
xkuznetc <466672@mail.muni.cz>
2017-12-29 20:19:33 +0100

    Security roles and filters added.

--- bandmanager-angular/src/main/java/cz/muni/fi/pa165/config/SecurityConfiguration.java
+++ bandmanager-angular/src/main/java/cz/muni/fi/pa165/config/SecurityConfiguration.java
@@ -1,10 +1,12 @@
 package cz.muni.fi.pa165.config;
 
+import cz.muni.fi.pa165.enums.SecurityRole;
 import cz.muni.fi.pa165.security.CustomAuthenticationProvider;
 import cz.muni.fi.pa165.security.RestAuthenticationEntryPoint;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,10 +37,61 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .exceptionHandling()
+        http.exceptionHandling()
                 .authenticationEntryPoint(restAuthenticationEntryPoint).and()
                 .authorizeRequests()
+
+                .antMatchers(HttpMethod.GET, "/rest/**").permitAll()
+
+                .antMatchers(HttpMethod.GET, "/rest/songs").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/songs").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/songs").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/songs").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/songs").hasRole(SecurityRole.MANAGER.name())
+
+                .antMatchers(HttpMethod.GET, "/rest/albums").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/albums").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/albums").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/albums").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/albums").hasRole(SecurityRole.MANAGER.name())
+                
+                .antMatchers(HttpMethod.GET, "/rest/bands").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/bands").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/bands").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/bands").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/bands").hasRole(SecurityRole.MANAGER.name())
+
+                .antMatchers(HttpMethod.GET, "/rest/bandinvites").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/bandinvites").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/bandinvites").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/bandinvites").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/bandinvites").hasRole(SecurityRole.MANAGER.name())
+
+                .antMatchers(HttpMethod.GET, "/rest/tours").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/tours").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/tours").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/tours").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/tours").hasRole(SecurityRole.MANAGER.name())
+
+                .antMatchers(HttpMethod.GET, "/rest/members").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/members").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/members").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/members").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/members").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/members").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/members").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/members").hasRole(SecurityRole.MEMBER.name())
+
+                .antMatchers(HttpMethod.GET, "/rest/managers").hasRole(SecurityRole.MEMBER.name())
+                .antMatchers(HttpMethod.GET, "/rest/managers").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.POST, "/rest/managers").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.PUT, "/rest/managers").hasRole(SecurityRole.MANAGER.name())
+                .antMatchers(HttpMethod.DELETE, "/rest/managers").hasRole(SecurityRole.MANAGER.name())
+
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/partials/**").permitAll()
+                .antMatchers("/index.html").permitAll()
+
                 .anyRequest().authenticated().and()
                 .formLogin().loginPage("/login.html").permitAll().and()
 
---
+++ bandmanager-persistance/src/main/java/cz/muni/fi/pa165/enums/SecurityRole.java
@@ -0,0 +1,10 @@
+package cz.muni.fi.pa165.enums;
+
+/**
+ * @author Iurii xkuznetc Kuznetcov
+ */
+
+public enum SecurityRole {
+    MEMBER,
+    MANAGER
+}
